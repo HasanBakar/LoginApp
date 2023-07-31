4592bc12e53dc0d978d2fa88a0d9c6431d7885db
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-07-31 20:02:03 +0600

    username page design completed

--- Server/index.js
+++ Server/index.js
@@ -5,9 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-// middleware
-app.use(cors());
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
